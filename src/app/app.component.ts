822f8c18847742f9068e465a0e9bce9b45a09625
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2024-03-29 19:05:22 +0100

    created routing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,22 @@
 import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
 import {HomeComponent} from './home/home.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
     <main>
+      <a [routerLink]="['/']">
+
     <header class="brand-name">
       <img class="brand-logo" aria-hidden="true" src="/assets/logo.svg"
            alt="logo">
     </header>
+      </a>
       <section class="content">
-        <app-home></app-home>
+        <router-outlet></router-outlet>
       </section>
   </main>`,
   styleUrl: './app.component.css',
